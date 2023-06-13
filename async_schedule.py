9ae78502a3b9b3c9a24e4c8cebc79cb0c1477916
Igor-lc <105611667+Igor-lc@users.noreply.github.com>
2023-06-13 10:20:19 +0300

    Add files via upload

--- async_schedule.py
+++ async_schedule.py
@@ -2,20 +2,24 @@ from time import sleep
 from time import time
 import asyncio
 
+
 def loader(url):
     print('Load {} at {:.2f}'.format(url, time() - start))
+
+
 async def spider(site_name):
     for page in range(1, 4):
-        #print(site_name, page)
+        # print(site_name, page)
         await asyncio.sleep(1)
         print(site_name, page)
+
 
 start = time()
 
 spiders = [asyncio.ensure_future(spider("Blog")),
            asyncio.ensure_future(spider("News")),
            asyncio.ensure_future(spider("Forum"))
-]
+           ]
 
 event_loop = asyncio.get_event_loop()
 now = event_loop.time()
